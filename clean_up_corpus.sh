02f794a9e0226e408095ec0d3fe8ed0dc661dee9
Menno van Zaanen <menno@abstractcow.com>
2026-08-04 13:13:40 +0200

    Moved to prepare_corpus.py

--- clean_up_corpus.sh
+++
@@ -1,15 +0,0 @@
-#!/bin/bash
-
-# This script takes a text file as input.  It assumes that the text
-# file is already tokenized by separating each token using whitespace.
-# The script then puts each word on a separate line and converts it to
-# lowercase.  It also removes empty lines.  It also only takes lines
-# with letters.  This also works for letters with diacritics.
-
-cat $1 \
-    | sed "s///g" \
-    | tr " " "\n" \
-    | tr '[:upper:]' '[:lower:]' \
-    | sed '/^\s*$/d' \
-    | grep "[a-z]" \
-    > $2
